de2f22995d1c1a2aa2117f59a45e3884f62edfe2
Nathan Hitchcock <nath9224@app-001.ord1.prod.github.rackspace.net>
2021-05-18 16:24:57 -0500

    add straight As question

---
+++ textgrids/straight_a/straight_As.py
@@ -0,0 +1,10 @@
+from TextGrid import TextGrid
+
+DEFAULT_FILENAME = 'guava.txt'
+
+def main():
+    grid = TextGrid(DEFAULT_FILENAME)
+    # TODO: your code here!
+    print(grid)
+
+if __name__ == '__main__':
